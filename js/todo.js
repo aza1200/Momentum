10b1f78bebfaa87f3699205698cddee76e8c4580
2018008068 <aza1200@naver.com>
2021-08-21 22:38:13 +0900

    toDoList

--- js/todo.js
+++ js/todo.js
@@ -3,15 +3,27 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+    //click 은 button 에서 일어났고 button 은 li 에 포함되어있다.
+    const li = event.target.parentElement;
+    li.remove();
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
+    span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click",deleteToDo);
 
     li.appendChild(span);
-    span.innerText = newTodo;
+    li.appendChild(button);
+
+    // list 안에 span 과 button 추가 한 이후에 ToDoList 에 자식 추가
+
     toDoList.appendChild(li);
 }
-
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
