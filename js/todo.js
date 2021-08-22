521b6e7eef6a60f2bc46ac5e415a319ecf4352f4
2018008068 <aza1200@naver.com>
2021-08-22 17:54:15 +0900

    add todos

--- js/todo.js
+++ js/todo.js
@@ -3,6 +3,14 @@ const toDoInput = toDoForm.querySelector("input");
 // const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
+}
+
 function deleteToDo(event){
     //click 은 button 에서 일어났고 button 은 li 에 포함되어있다.
     const li = event.target.parentElement;
@@ -28,8 +36,24 @@ function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
-//sex
+
+function sayHello(item){
+    console.log(item);
+}
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+//console.log(savedToDos);
+if(savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    //parsedToDos.forEach(sayHello);
+    //sayHello("a");
+
+    parsedToDos.forEach((item)=> console.log("Hello "+ item));
+
+}
